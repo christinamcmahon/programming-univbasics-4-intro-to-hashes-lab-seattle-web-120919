d7157675fe9b56f686459bf284dac4016295ecc6
Christina McMahon <christina.mcmahon1@gmail.com>
2019-11-18 23:15:42 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -25,8 +25,8 @@ end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
-  if hash[:key]
-    return hash[:key]
+  if hash[key]
+    return hash[key]
   end
   nil
 end
